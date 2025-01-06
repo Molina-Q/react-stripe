79392e8f0338853c3b36536ac60fcc1990271d3b
Molina-Q <quentinmolina1@gmail.com>
2025-01-06 16:15:22 +0100

    Wrap App component with ProductsContextProvider for improved state management

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css';
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css';
+import ProductsContextProvider from './context/ProductsContextProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+    <ProductsContextProvider>
       <App />
+    </ProductsContextProvider>
     </BrowserRouter>
   </StrictMode>,
 )
