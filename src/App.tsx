8ae11fdfe51caea71801ac4af304e5e26593499e
Molina-Q <quentinmolina1@gmail.com>
2025-01-07 14:13:23 +0100

    Add FeaturedCollection component to App for enhanced product display

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.scss'
+import FeaturedCollection from './components/featured-collection/featured-collection'
 import Header from './components/header/Header'
 import Hero from './components/hero/Hero'
 import MainSection from './components/main-section/main-section'
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <MainSection />
+      <FeaturedCollection />
     </div>
   )
 }
