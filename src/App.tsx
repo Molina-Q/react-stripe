c27d9320c0c740a92b0d1234d4db40fe46d294aa
Molina-Q <quentinmolina1@gmail.com>
2025-01-07 15:29:15 +0100

    Add routes for Shop and SingleProduct components in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from './components/home-page'
 import './App.scss'
 import NotFound from './components/not-found'
+import Shop from './components/pages/shop/shop'
+import SingleProduct from './components/single-product/single-product'
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/product/:id' element={<SingleProduct />} />
+
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
