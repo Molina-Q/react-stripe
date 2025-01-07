85859b5385f13c87624f5c9c909b797fe6558285
Molina-Q <quentinmolina1@gmail.com>
2025-01-07 14:13:30 +0100

    Add FeaturedCollection component to display a curated selection of products

---
+++ src/components/featured-collection/featured-collection.tsx
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react'
+import { ProductsContext } from '../../context/ProductsContextProvider'
+import FeaturedProduct from '../shared/featured-product';
+
+interface ProductType {
+    id: number;
+    title: string;
+    description: string;
+    imageUrl: string;
+    price: number;
+}
+
+const FeaturedCollection = () => {
+    const { products } = useContext(ProductsContext);
+    const productItems = products.filter((product, i) => i < 4).map( product => (
+        <FeaturedProduct key={product.id} product={product} />
+    ));
+
+    return (
+        <div className='featured-collection container'>
+            <h2 className='featured-section-title'>Featured Collection</h2>
+
+            <div className='products'>
+                {productItems}
+            </div>
+        </div>
+    )
+}
+
+export default FeaturedCollection
