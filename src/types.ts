c4c08006be1cd1d288ac7f9ad9ca9c24064e45a8
Molina-Q <quentinmolina1@gmail.com>
2025-01-08 15:24:27 +0100

    Add CartItemType and CartType interfaces to define cart structure

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+export interface CartItemType {
+    id: number;
+    quantity: number;
+}
+
+export type CartType = {
+    cartItems?: CartItemType[];
+    itemCount: number;
+    total: number;
+};
+
+export interface ProductType {
+    id: number;
+    title: string;
+    description: string;
+    imageUrl: string;
+    price: number;
+    quantity?: number;
+}
