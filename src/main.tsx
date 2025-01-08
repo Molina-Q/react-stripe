1c5c72781d1044b35cbb9b9e14bd7f39b3d7df7c
Molina-Q <quentinmolina1@gmail.com>
2025-01-08 11:26:20 +0100

    Add CartContextProvider to main application for cart state management

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import ProductsContextProvider from './context/ProductsContextProvider.tsx';
+import CartContextProvider from './context/CartContextProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
     <ProductsContextProvider>
+    <CartContextProvider>
       <App />
+    </CartContextProvider>
     </ProductsContextProvider>
     </BrowserRouter>
   </StrictMode>,
