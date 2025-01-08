bf51079d47f3b90be2819b992810bbd41eaa125b
Molina-Q <quentinmolina1@gmail.com>
2025-01-08 15:24:31 +0100

    Update FeaturedProduct component to manage cart actions and display button based on product availability in cart

--- src/components/shared/featured-product.tsx
+++ src/components/shared/featured-product.tsx
@@ -1,11 +1,16 @@
-import { ProductType } from '../../context/ProductsContextProvider';
+import { useContext } from 'react';
+import { CartContext } from '../../context/CartContextProvider';
+import { ProductType } from '../../types';
 import './featured-product.styles.scss'
+import { isInCart } from '../../helper';
 
 interface FeaturedProductProps {
     product: ProductType;
 }
 
 const FeaturedProduct: React.FC<FeaturedProductProps> = ({ product }) => {
+    const { addProduct, cartItems } = useContext(CartContext);
+
     return (
         <div className='featured-product'>
             <div className='featured-image'>
@@ -14,7 +19,24 @@ const FeaturedProduct: React.FC<FeaturedProductProps> = ({ product }) => {
             <div className='name-price'>
                 <h3>{product.title}</h3>
                 <p>$ {product.price}</p>
-                <button className='button is-black nomad-btn'>ADD TO CART</button>
+                {
+                    !isInCart(product, cartItems) &&
+                    <button
+                        className='button is-black nomad-btn'
+                        onClick={() => addProduct(product)}
+                    >
+                        ADD TO CART
+                    </button>
+                }
+                {
+                    isInCart(product, cartItems) &&
+                    <button
+                        className='button is-black nomad-btn'
+                        onClick={() => addProduct(product)}
+                    >
+                        ADD MORE
+                    </button>
+                }
             </div>
         </div>
     )
