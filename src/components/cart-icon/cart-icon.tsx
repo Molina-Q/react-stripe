8fd8c334c8d8cf0cfbb76ec26201dff1888d791f
Molina-Q <quentinmolina1@gmail.com>
2025-01-08 11:26:25 +0100

    Update CartIcon component to display dynamic item count from CartContext

--- src/components/cart-icon/cart-icon.tsx
+++ src/components/cart-icon/cart-icon.tsx
@@ -1,11 +1,14 @@
+import { useContext } from 'react';
 import shoppingBag from '../../assets/shopping-bag.png';
 import './cart-icon.styles.scss';
+import { CartContext } from '../../context/CartContextProvider';
 
 const CartIcon = () => {
+  const { itemCount } = useContext(CartContext)
   return (
     <div className='cart-container'>
         <img src={shoppingBag} alt="shopping bag" />
-        <span className='cart-count'> 5</span>
+        <span className='cart-count'> {itemCount > 0 ? itemCount : 0}</span>
     </div>
   )
 }
