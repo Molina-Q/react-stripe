ab93c7f92772a567ca8e071156301bee482e9c0b
Molina-Q <quentinmolina1@gmail.com>
2025-01-08 15:24:45 +0100

    Implement ADD_ITEM action in cartReducer to manage item addition and calculate total items and price

--- src/context/CartReducer.ts
+++ src/context/CartReducer.ts
@@ -1,9 +1,33 @@
+import { ProductType } from "../types";
+
 interface CartReducerProps {
     state: any;
     action: any;
 }
+
+const sumItems = (cartItems: ProductType[]) => {
+    const itemCount = cartItems.reduce((total: number, product: ProductType) => total + (product.quantity ? product.quantity : 0), 0);
+    const total = cartItems.reduce((total: number, product: ProductType) => total + product.price * (product.quantity ? product.quantity : 0), 0);
+    return { itemCount, total };
+}
+
 const cartReducer = ({ state, action }: CartReducerProps) => {
     switch (action.type) {
+
+        case 'ADD_ITEM':
+            // check if item is in cart
+            if (!state.cartItems.find((item: ProductType) => item.id === action.payload.id)) {
+                state.cartItems.push({
+                    ...action.payload,
+                    quantity: 1,
+                })
+            }
+
+            return {
+                ...state,
+                cartItems: [...state.cartItems],
+                ...sumItems(state.cartItems)
+            }
 
         default:
             return state;
