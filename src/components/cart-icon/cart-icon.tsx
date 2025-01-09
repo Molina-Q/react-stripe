3459a10ce79130de283ac797e8417a2c7ffa4096
Molina-Q <quentinmolina1@gmail.com>
2025-01-09 14:22:58 +0100

    Add click navigation to CartIcon for accessing the cart page

--- src/components/cart-icon/cart-icon.tsx
+++ src/components/cart-icon/cart-icon.tsx
@@ -2,11 +2,13 @@ import { useContext } from 'react';
 import shoppingBag from '../../assets/shopping-bag.png';
 import './cart-icon.styles.scss';
 import { CartContext } from '../../context/CartContextProvider';
+import { useNavigate } from 'react-router-dom';
 
 const CartIcon = () => {
   const { itemCount } = useContext(CartContext)
+  const navigate = useNavigate();
   return (
-    <div className='cart-container'>
+    <div className='cart-container' onClick={() => navigate('/cart')}>
         <img src={shoppingBag} alt="shopping bag" />
         <span className='cart-count'> {itemCount > 0 ? itemCount : 0}</span>
     </div>
