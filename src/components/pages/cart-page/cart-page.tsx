5a26cdc5f7dc85806e7735024fe13c04a3e39beb
Molina-Q <quentinmolina1@gmail.com>
2025-01-09 11:20:48 +0100

    Add CartPage component for displaying the shopping cart and its contents

---
+++ src/components/pages/cart-page/cart-page.tsx
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react';
+
+import Layout from '../../shared/layout';
+
+import './cart-page.styles.scss';
+import { CartContext } from '../../../context/CartContextProvider';
+import Total from './total';
+import CartItem from './cart-item';
+
+const CartPage = () => {
+    const { cartItems, itemCount, total, increase, decrease, removeProduct, clearCart } = useContext(CartContext);
+    const funcs = { increase, decrease, removeProduct }
+    return (
+        <Layout>
+            <>
+                <h1>Cart</h1>
+                {
+                    cartItems.length === 0 ? <div className='empty-cart'>Your Cart is empty</div>
+                        :
+                        <>
+                            <div className='cart-page'>
+                                <div className='cart-item-container'>
+                                    {
+                                        cartItems.map(item => <CartItem {...item} key={item.id} {...funcs} />)
+                                    }
+                                </div>
+                                <Total itemCount={itemCount} total={total} clearCart={clearCart} />
+                            </div>
+                        </>
+                }
+            </>
+        </Layout>
+    );
+}
+
+export default CartPage;
