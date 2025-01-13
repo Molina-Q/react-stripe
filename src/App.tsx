c58d0dbd7fc614439d97ca45dc8e2c488610d57f
Molina-Q <quentinmolina1@gmail.com>
2025-01-13 15:34:30 +0100

    Add Checkout route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import NotFound from './components/not-found'
 import Shop from './components/pages/shop/shop'
 import SingleProduct from './components/single-product/single-product'
 import CartPage from './components/pages/cart-page/cart-page'
+import Checkout from './components/checkout/checkout'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path='/shop' element={<Shop />} />
         <Route path='/cart' element={<CartPage />} />
         <Route path='/product/:id' element={<SingleProduct />} />
+        <Route path='/checkout' element={<Checkout />} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
