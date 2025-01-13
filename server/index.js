e3e4c0774cd5d45cad73cb5cc203e37cde53598d
Molina-Q <quentinmolina1@gmail.com>
2025-01-13 16:01:40 +0100

    Refine CORS configuration to restrict allowed origins and specify HTTP methods

--- server/index.js
+++ server/index.js
@@ -7,9 +7,21 @@ const app = express();
 const port = 8080;
 
 app.use(express.json());
-app.use(cors({
-    origin: 'true'
-}));
+const corsOptions = {
+    origin: (origin, callback) => {
+        const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://product.wave-auth.com'];
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type'],
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
     res.send('Hello World');
