1db19e227aacc3d38858877b6f490f31b8b40f33
Molina-Q <quentinmolina1@gmail.com>
2025-01-13 15:38:51 +0100

    Add fetchFromAPI function to handle API requests

--- src/helper.ts
+++ src/helper.ts
@@ -4,3 +4,24 @@ export const isInCart = (product: ProductType, cart: CartItemType[] | undefined)
     if (!cart) return false;
     return cart.find((item) => item.id === product.id);
 }
+
+const API = 'http://localhost:8080';
+
+interface FetchOptions {
+    method: 'GET' | 'POST' | 'PUT' | 'DELETE';
+    body: any;
+  }
+  
+  export async function fetchFromAPI(endpoint: string, opts?: FetchOptions): Promise<any> {
+    const { method, body } = { method: 'POST', body: null, ...opts };
+
+    const res = await fetch(`${API}/${endpoint}`, {
+        method,
+        ...(body && { body: JSON.stringify(body) }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    return res.json();
+  }
