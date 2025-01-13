9ad7684f404a44ae5a5a8bf0302addb5bdfdf277
Molina-Q <quentinmolina1@gmail.com>
2025-01-13 15:19:38 +0100

    Wrap App component with Stripe Elements provider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,22 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import ProductsContextProvider from './context/ProductsContextProvider.tsx';
 import CartContextProvider from './context/CartContextProvider.tsx';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-    <ProductsContextProvider>
-    <CartContextProvider>
-      <App />
-    </CartContextProvider>
-    </ProductsContextProvider>
+      <ProductsContextProvider>
+        <CartContextProvider>
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
+        </CartContextProvider>
+      </ProductsContextProvider>
     </BrowserRouter>
   </StrictMode>,
 )
