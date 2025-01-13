7399fd586219bae68fcf6bbc4b388c59eedb1608
Molina-Q <quentinmolina1@gmail.com>
2025-01-13 16:01:44 +0100

    Integrate StripeCheckout component into Checkout page

--- src/components/checkout/checkout.tsx
+++ src/components/checkout/checkout.tsx
@@ -4,6 +4,7 @@ import React, { useContext, useState } from 'react';
 import './checkout.styles.scss';
 import { CartContext } from '../../context/CartContextProvider';
 import Layout from '../shared/layout';
+import StripeCheckout from './stripe-checkout/stripe-checkout';
 
 const Checkout = () => {
     const { itemCount, total, cartItems } = useContext(CartContext);
@@ -20,6 +21,7 @@ const Checkout = () => {
                 <h2>Checkout Summary</h2>
                 <h3>{`Total Items: ${itemCount}`}</h3>
                 <h4>{`Amount to Pay: $${total}`}</h4>
+                <StripeCheckout />
                 {/* <div style={addressShown}> */}
                     {/* <ShippingAddress setShipping={setShipping} /> */}
                 {/* </div> */}
