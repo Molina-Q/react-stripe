496dda681368e76fce2bb756a056a4bba0490da1
Molina-Q <quentinmolina1@gmail.com>
2025-01-14 14:54:38 +0100

    Add webhook endpoint and improve CORS configuration in server

--- server/index.js
+++ server/index.js
@@ -1,41 +1,46 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config({ path: './.env' });
-const createCheckoutSession = require('./api/checkout');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config({ path: "./.env" });
+const createCheckoutSession = require("./api/checkout");
+const webhook = require("./api/webhook");
 
 const app = express();
 const port = 8080;
 
-app.use(express.json({
-    verify: (req, res, buf) => {
-        req.rawBody = Buffer,
-    }
-}));
+app.use(
+	express.json({
+		verify: (req, res, buffer) => (req["rawBody"] = buffer),
+	})
+);
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://product.wave-auth.com'];
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type'],
-    optionsSuccessStatus: 200
+	origin: (origin, callback) => {
+		const allowedOrigins = [
+			"http://localhost:5173",
+			"http://127.0.0.1:5173",
+			"https://product.wave-auth.com",
+		];
+		if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+	methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+	allowedHeaders: ["Content-Type"],
+	optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+	res.send("Hello World");
 });
 
+app.post("/create-checkout-session", createCheckoutSession);
 
-app.post('/create-checkout-session', createCheckoutSession);
-
+app.post("/webhook", webhook);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
