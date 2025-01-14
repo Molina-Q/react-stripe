567de707c84fec559acf51b38f6a6ad91928e0d7
Molina-Q <quentinmolina1@gmail.com>
2025-01-14 12:27:42 +0100

    Add routes for success and canceled pages in checkout flow

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Shop from './components/pages/shop/shop'
 import SingleProduct from './components/single-product/single-product'
 import CartPage from './components/pages/cart-page/cart-page'
 import Checkout from './components/checkout/checkout'
+import Success from './components/checkout/stripe-checkout/success'
+import Canceled from './components/checkout/stripe-checkout/cancel'
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Route path='/cart' element={<CartPage />} />
         <Route path='/product/:id' element={<SingleProduct />} />
         <Route path='/checkout' element={<Checkout />} />
+        <Route path='/success' element={<Success />} />
+        <Route path='/canceled' element={<Canceled />} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
