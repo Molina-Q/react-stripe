4fa4091c170dc03c323a234752db4a6223c590e5
Molina-Q <quentinmolina1@gmail.com>
2025-01-14 15:16:44 +0100

    Add function to calculate total order amount for payment intent

--- server/api/paymentIntent.js
+++ server/api/paymentIntent.js
@@ -1,5 +1,22 @@
 const stripeAPI = require("../stripe.js");
 
+/**
+ * @typedef {Object} CartItem
+ * @property {number} price - The price of the item
+ * @property {number} quantity - The quantity of the item
+ */
+
+/**
+ * Calculates the total order amount in cents
+ * @param {CartItem[]} cartItems - Array of cart items
+ * @returns {number} Total amount in cents
+ */
+function calculateOrderAmount(cartItems) {
+    return cartItems.reduce((total, product) => {
+        return total + product.price * product.quantity;
+    }, 0) * 100;
+}
+
 async function paymentIntent(req, res) {
 	const { cartItems, description, receipt_email, shipping } = req.body;
 
