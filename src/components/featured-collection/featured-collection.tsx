7a13e628b467e94a7e483a4530620bcc079d19a3
Molina-Q <quentinmolina1@gmail.com>
2025-01-15 16:15:02 +0100

    Refactor featured-collection component to simplify product selection logic

--- src/components/featured-collection/featured-collection.tsx
+++ src/components/featured-collection/featured-collection.tsx
@@ -4,16 +4,16 @@ import FeaturedProduct from '../shared/featured-product';
 
 const FeaturedCollection = () => {
     const { products } = useContext(ProductsContext);
-    const productItems = products.filter((product, i) => i < 4).map(product => (
-        <FeaturedProduct key={product.id} product={product} />
-    ));
+    const featuredProducts = products.slice(0, 4);
 
     return (
         <div className='featured-collection container'>
             <h2 className='featured-section-title'>Featured Collection</h2>
 
             <div className='products'>
-                {productItems}
+                {featuredProducts.map(product => (
+                    <FeaturedProduct key={product.id} product={product} />
+                ))}
             </div>
         </div>
     )
