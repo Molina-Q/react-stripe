8c20a919639eb5a9c69a1930cb9a5e132adf2df3
Molina-Q <quentinmolina1@gmail.com>
2025-01-15 10:18:31 +0100

    Add webhook handlers for Stripe events to improve payment processing logging

--- server/api/webhook.js
+++ server/api/webhook.js
@@ -1,5 +1,18 @@
 const stripeAPI = require('../stripe.js');
 
+const webhookHandlers = {
+    'checkout.session.completed': (data) => {
+        console.log('Checkout session completed!');
+        const session = data.object;
+    },
+    'payment_intent.succeeded': (data) => {
+        console.log('Payment was successful!');
+    },
+    'payment_intent.payment_failed': (data) => {
+        console.log('Payment failed!');
+    }
+}
+
 function webhook(req, res) {
     const sig = req.headers['stripe-signature'];
 
@@ -16,8 +29,10 @@ function webhook(req, res) {
         return res.status(400).send(`Webhook Error: ${err.message}`);
     }
 
-    if (event.type === 'checkout.session.completed') {
+    if (webhookHandlers[event.type]) {
+        webhookHandlers[event.type](event.data.object);
         const session = event.data.object;
+        console.log(session);
     }
 }
 
