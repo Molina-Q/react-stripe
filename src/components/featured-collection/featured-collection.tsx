18c6e8e66ee740eef73df02df984afb4497b2ded
Molina-Q <quentinmolina1@gmail.com>
2025-01-15 16:13:07 +0100

    Fix formatting in featured-collection component for consistency

--- src/components/featured-collection/featured-collection.tsx
+++ src/components/featured-collection/featured-collection.tsx
@@ -4,7 +4,7 @@ import FeaturedProduct from '../shared/featured-product';
 
 const FeaturedCollection = () => {
     const { products } = useContext(ProductsContext);
-    const productItems = products.filter((product, i) => i < 4).map( product => (
+    const productItems = products.filter((product, i) => i < 4).map(product => (
         <FeaturedProduct key={product.id} product={product} />
     ));
 
