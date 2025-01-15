cd91463eeeeea964e85fac31655d32be2bcc9a6c
Molina-Q <quentinmolina1@gmail.com>
2025-01-15 10:00:19 +0100

    Integrate CustomCheckout component into Checkout for conditional rendering based on shipping state

--- src/components/checkout/checkout.tsx
+++ src/components/checkout/checkout.tsx
@@ -6,6 +6,7 @@ import { CartContext } from '../../context/CartContextProvider';
 import Layout from '../shared/layout';
 import StripeCheckout from './stripe-checkout/stripe-checkout';
 import ShippingAddress, { ShippingValues } from './custom-checkout/shipping-address';
+import CustomCheckout from './custom-checkout/custom-checkout';
 
 const Checkout = () => {
     const { itemCount, total, cartItems } = useContext(CartContext);
@@ -13,9 +14,11 @@ const Checkout = () => {
     const addressShown = {
         display: (shipping ? 'none' : 'block')
     }
-    // const cardShown = {
-    //     display: (shipping ? 'block' : 'none')
-    // }
+
+    const cardShown = {
+        display: (shipping ? 'block' : 'none')
+    }
+
     return (
         <Layout>
             <div className='checkout'>
@@ -26,9 +29,9 @@ const Checkout = () => {
                 <div style={addressShown}>
                     <ShippingAddress setShipping={setShipping} />
                 </div>
-                {/* <div style={cardShown}> */}
-                    {/* <CustomCheckout {...{ shipping, cartItems }} /> */}
-                {/* </div> */}
+                <div style={cardShown}>
+                    <CustomCheckout shipping={shipping} cartItems={cartItems!} />
+                </div>
             </div>
         </Layout>
     );
