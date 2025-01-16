21e86cbf62e1fb71c0e2b167cd7b511dab1be1b1
Molina-Q <quentinmolina1@gmail.com>
2025-01-16 10:48:47 +0100

    Refactor Firebase initialization to use compat SDK and add user profile document creation function

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,23 +1,52 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import "firebase/firestore";
-import { getFirestore } from "firebase/firestore/lite";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
 
-// Your web app's Firebase configuration
+interface AdditionalData {
+    [key: string]: any;
+}
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIRESTORE_API_KEY,
-  authDomain: "react-stripe-fa0d9.firebaseapp.com",
-  projectId: "react-stripe-fa0d9",
-  storageBucket: "react-stripe-fa0d9.firebasestorage.app",
-  messagingSenderId: "566117766748",
-  appId: "1:566117766748:web:baaf07acf38e4ab77e3899"
+    apiKey: import.meta.env.VITE_FIRESTORE_API_KEY,
+    authDomain: "react-stripe-fa0d9.firebaseapp.com",
+    projectId: "react-stripe-fa0d9",
+    storageBucket: "react-stripe-fa0d9.firebasestorage.app",
+    messagingSenderId: "566117766748",
+    appId: "1:566117766748:web:baaf07acf38e4ab77e3899"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-const firestore = getFirestore(app);
+const firestore = firebase.firestore();
 
-export {
-    firestore,
+const auth = firebase.auth();
+
+const createUserProfileDocument = async (
+    userAuth: firebase.User | null,
+    additionalData?: AdditionalData
+): Promise<firebase.firestore.DocumentReference | undefined> => {
+    if (!userAuth) { return; }
+    
+    const userRef = firestore.doc(`users/${userAuth.uid}`);  // Changed from userAuth.multiFactor.user.uid to userAuth.uid
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData,
+            });
+        } catch (error) {
+            console.log('error creating user', (error as Error).message);
+        }
+    }
+    return userRef;
 };
+
+export { firestore, createUserProfileDocument, auth };
